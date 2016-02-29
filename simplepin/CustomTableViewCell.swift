cbe83597eb43b4b28f8b1aa16390379a75cb2d44
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2016-02-29 22:34:35 +0200

    Try to get dynamic table to work

---
+++ simplepin/CustomTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomTableViewCell.swift
+//  simplepin
+//
+//  Created by Mathias Lindholm on 29.2.2016.
+//  Copyright © 2016 Mathias Lindholm. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- simplepin/ViewController.swift
+++ simplepin/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var bookmarks:[String: String] = ["The Grand Del Mar": "5300 Grand Del Mar Court, San Diego, CA 92130",
+        "French Quarter Inn": "166 Church St, Charleston, SC 29401",
+        "Bardessono": "6526 Yount Street, Yountville, CA 94599",
+        "Hotel Yountville": "6462 Washington Street, Yountville, CA 94599",
+        "Islington Hotel": "321 Davey Street, Hobart, Tasmania 7000, Australia",
+        "The Henry Jones Art Hotel": "25 Hunter Street, Hobart, Tasmania 7000, Australia",
+        "Clarion Hotel City Park Grand": "22 Tamar Street, Launceston, Tasmania 7250, Australia",
+        "Quality Hotel Colonial Launceston": "31 Elizabeth St, Launceston, Tasmania 7250, Australia",
+        "Premier Inn Swansea Waterfront": "Waterfront Development, Langdon Rd, Swansea SA1 8PL, Wales",
+        "Hatcher's Manor": "73 Prossers Road, Richmond, Clarence, Tasmania 7025, Australia"]
+    
+    var bookmarkNames:[String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        bookmarkNames = [String](bookmarks.keys)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookmarks.count
+    }
 
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "CustomTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! CustomTableViewCell
+
+        // Fetches the appropriate meal for the data source layout.
+        let bookmarkName = bookmarkNames[indexPath.row]
+
+        cell.titleLabel.text = bookmarkName
+        cell.descriptionLabel.text = bookmarks[bookmarkName]
+
+        return cell
+    }
 }
-
