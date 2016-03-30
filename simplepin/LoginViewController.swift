729510631ed4490ab02066546a829b869061f128
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2016-03-30 23:47:51 +0300

    Add login view

---
+++ simplepin/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  simplepin
+//
+//  Created by Mathias Lindholm on 30.03.2016.
+//  Copyright © 2016 Mathias Lindholm. All rights reserved.
+//
+
+import UIKit
+
+class LoginModalViewController: UIViewController {
+    var fetchApiTokenTask: NSURLSessionTask?
+    let defaults = NSUserDefaults.standardUserDefaults()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
