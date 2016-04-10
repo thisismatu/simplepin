42b32c139d1f3c554e552c6c1528f946b3ebf0ed
Mathias Lindholm <mathias.lindholm@gmail.com>
2016-04-11 00:00:09 +0300

    Add basic textview placholder
    This still needs work

--- simplepin/AddBookmarkTableViewController.swift
+++ simplepin/AddBookmarkTableViewController.swift
@@ -8,19 +8,37 @@
 
 import UIKit
 
-class AddBookmarkTableViewController: UITableViewController {
+class AddBookmarkTableViewController: UITableViewController, UITextViewDelegate {
     let defaults = NSUserDefaults.standardUserDefaults()
 
     @IBOutlet var privateBokkmarkSwitch: UISwitch!
     @IBOutlet var toreadBookmarkSwitch: UISwitch!
+    @IBOutlet var descriptionTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        descriptionTextView.delegate = self
+
         if (defaults.boolForKey("privateByDefault") == true) {
             privateBokkmarkSwitch.on = true
         }
+
     }
+
+    func textViewDidBeginEditing(view: UITextView) {
+        if !descriptionTextView.text.isEmpty {
+            descriptionTextView.backgroundColor = UIColor.whiteColor()
+        }
+    }
+
+    func textViewDidEndEditing(textView: UITextView) {
+        if descriptionTextView.text.isEmpty {
+            descriptionTextView.backgroundColor = UIColor.clearColor()
+        }
+    }
+
+    // TODO: Fix the placeholder https://grokswift.com/uitextview-placeholder/ https://codedump.io/share/Pt7eXF6JKQf/1/text-view-placeholder-swift
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
