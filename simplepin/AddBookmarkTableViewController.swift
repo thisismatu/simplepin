ba857771c6531c434725f90c6a533706352dfb38
Mathias Lindholm <mathias.lindholm@gmail.com>
2016-04-14 21:46:15 +0300

    Extend add bookmark functionality to include tags, toread and shared values

--- simplepin/AddBookmarkTableViewController.swift
+++ simplepin/AddBookmarkTableViewController.swift
@@ -10,30 +10,51 @@ import UIKit
 
 class AddBookmarkTableViewController: UITableViewController, UITextViewDelegate {
     var addBookmarkTask: NSURLSessionTask?
+    var toreadValue = "no"
+    var sharedValue = "yes"
     let defaults = NSUserDefaults.standardUserDefaults()
 
-    @IBOutlet var privateBokkmarkSwitch: UISwitch!
-    @IBOutlet var toreadBookmarkSwitch: UISwitch!
+    @IBOutlet var privateSwitch: UISwitch!
+    @IBOutlet var toreadSwitch: UISwitch!
     @IBOutlet var urlTextField: UITextField!
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var tagsTextField: UITextField!
     @IBOutlet var addButton: UIBarButtonItem!
+
+    @IBAction func toreadSwitchPressed(sender: AnyObject) {
+        if (toreadSwitch.on == true) {
+            toreadValue = "yes"
+        } else if (toreadSwitch.on == false) {
+            toreadValue = "no"
+        }
+    }
+
+    @IBAction func privateSwitchPressed(sender: AnyObject) {
+        if (privateSwitch.on == true) {
+            sharedValue = "no"
+        } else if (privateSwitch.on == false) {
+            sharedValue = "yes"
+        }
+
+    }
 
     @IBAction func addButtonPressed(sender: AnyObject) {
 
         guard let urlText = urlTextField.text,
             let url = NSURL(string: urlText),
             let title = titleTextField.text,
-            let description = descriptionTextView.text else {
+            let description = descriptionTextView.text,
+            let tags = tagsTextField.text?.componentsSeparatedByString(" ") else {
                 return
         }
 
         if urlText.isEmpty || title.isEmpty {
-            self.alertError("Please provide ULR and title", message: nil)
+            self.alertError("Please provide URL and Title", message: nil)
             return
         }
 
-        self.addBookmarkTask = Network.addBookmark(url, title: title, description: description) { resultCode in
+        self.addBookmarkTask = Network.addBookmark(url, title: title, description: description, tags: tags, shared: sharedValue, toread: toreadValue ) { resultCode in
             if resultCode == "done" {
                 print(resultCode)
             } else {
@@ -49,7 +70,8 @@ class AddBookmarkTableViewController: UITableViewController, UITextViewDelegate 
         descriptionTextView.delegate = self
 
         if (defaults.boolForKey("privateByDefault") == true) {
-            privateBokkmarkSwitch.on = true
+            privateSwitch.on = true
+            sharedValue = "no"
         }
 
     }
