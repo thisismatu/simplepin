e8d7e497cc271304caff107f51a80e6913c14f3b
Mathias Lindholm <mathias.lindholm@gmail.com>
2016-07-05 14:50:06 +0300

    Get page url and add content validation

--- Add Bookmark/ShareViewController.swift
+++ Add Bookmark/ShareViewController.swift
@@ -12,14 +12,26 @@ import Social
 class ShareViewController: SLComposeServiceViewController {
 
     override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
+        if contentText.isEmpty {
+            return false
+        }
         return true
     }
 
     override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = item.attachments?.first as? NSItemProvider {
+                if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                    itemProvider.loadItemForTypeIdentifier("public.url", options: nil, completionHandler: { (url, error) -> Void in
+                        if let shareURL = url as? NSURL {
+                            print(shareURL)
+                            print(self.contentText)
+                        }
+                        self.extensionContext?.completeRequestReturningItems([], completionHandler:nil)
+                    })
+                }
+            }
+        }
     }
 
     override func configurationItems() -> [AnyObject]! {
