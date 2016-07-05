f5d14b84f0353617fa2dd0e7aad4bd379fbcce39
Mathias Lindholm <mathias.lindholm@gmail.com>
2016-07-05 13:54:15 +0300

    Change NSExtensionActivationRule

--- Add Bookmark/ShareViewController.swift
+++ Add Bookmark/ShareViewController.swift
@@ -18,7 +18,6 @@ class ShareViewController: SLComposeServiceViewController {
 
     override func didSelectPost() {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
     }
@@ -27,5 +26,4 @@ class ShareViewController: SLComposeServiceViewController {
         // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
         return []
     }
-
 }
