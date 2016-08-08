939db5614ffef65e2ca88789ace4072eb3f48432
Mathias Lindholm <mathias.lindholm@gmail.com>
2016-08-08 13:29:03 +0300

    Change timeout from 5 to 15 seconds

--- simplepin/Network.swift
+++ simplepin/Network.swift
@@ -19,7 +19,7 @@ struct Network {
     static let userToken = defaults.stringForKey("userToken")
 
     static func session() -> NSURLSession {
-        config.timeoutIntervalForRequest = 5.0
+        config.timeoutIntervalForRequest = 15.0
         return NSURLSession(configuration: config)
     }
 
