f29349b1c7e2a2afdc789d5cc15ca346deced89e
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2018-08-09 11:18:51 +0300

    Change textfield placeholder color

--- simplepin/views/InputField.swift
+++ simplepin/views/InputField.swift
@@ -4,8 +4,20 @@ class InputField: UITextField {
 
     let textInset = UIEdgeInsets(top: 13.5, left: 12, bottom: 13.5, right: 12)
 
+    override var placeholder: String? {
+        didSet {
+            guard let text = placeholder else {
+                self.attributedPlaceholder = NSAttributedString(string: "")
+                return
+            }
+            let attributedText = NSAttributedString(string: text, attributes: [NSAttributedStringKey.foregroundColor: UIColor.simplepin_gray2])
+            self.attributedPlaceholder = attributedText
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: .zero)
+
         self.layer.cornerRadius = 4
         self.layer.borderWidth = 1
         self.layer.borderColor = UIColor.simplepin_border.cgColor
