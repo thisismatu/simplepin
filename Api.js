4f61d53b27ede3050c44f72295e0acff041e9999
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-10-21 22:41:27 +0300

    Add scaffolding for update and posts requests

--- Api.js
+++ Api.js
@@ -3,6 +3,8 @@ import queryString from 'query-string'
 const server = 'https://api.pinboard.in/v1'
 
 const loginUrl = (parameters) => `${server}/user/api_token/?${parameters}`
+const updateUrl = (parameters) => `${server}/posts/update/?${parameters}`
+const postsUrl = (parameters) => `${server}/posts/all/?${parameters}`
 
 const handleResponse = (response) => {
   if (!response.ok) {
@@ -24,4 +26,22 @@ export const login = (token) => {
   }
   const params = queryString.stringify(data)
   return fetchWithErrorHandling(loginUrl(params))
+}
+
+export const update = (token) => {
+ const data = {
+   'format': 'json',
+   'auth_token': token
+ }
+ const params = queryString.stringify(data)
+ return fetchWithErrorHandling(updateUrl(params))
+}
+
+export const posts = (token) => {
+ const data = {
+   'format': 'json',
+   'auth_token': token
+ }
+ const params = queryString.stringify(data)
+ return fetchWithErrorHandling(postsUrl(params))
 }
