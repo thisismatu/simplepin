5a8cf235164bf3fb8187a1a892d00fefb00606fd
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-11-11 11:26:30 +0200

    Make menu button stateless component

--- components/MenuButton.js
+++ components/MenuButton.js
@@ -2,17 +2,15 @@ import React from 'react'
 import {StyleSheet, TouchableOpacity, Image} from 'react-native'
 import Base from 'app/assets/Base'
 
-export default class MenuButton extends React.PureComponent {
-  render() {
-    return (
-      <TouchableOpacity
-        activeOpacity={0.7}
-        onPress={() => this.props.navigation.openDrawer()}
-      >
-        <Image source={require('app/assets/ic-menu.png')} style={styles.menuButton} />
-      </TouchableOpacity>
-    )
-  }
+const MenuButton = ({navigation}) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={() => navigation.openDrawer()}
+    >
+      <Image source={require('app/assets/ic-menu.png')} style={styles.menuButton} />
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -22,3 +20,5 @@ const styles = StyleSheet.create({
     tintColor: Base.colors.blue2,
   }
 })
+
+export default MenuButton
