1bb8327c505d1fe1ac8747404a59b7985e3f06a6
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-11-12 19:02:49 +0200

    Finally proper styling to switch component

--- views/SettingsView.js
+++ views/SettingsView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, Text, View, Switch, TouchableOpacity, ScrollView, SafeAreaView} from 'react-native'
+import {StyleSheet, Text, View, Switch, TouchableOpacity, ScrollView, SafeAreaView, Platform} from 'react-native'
 import MenuButton from 'app/components/MenuButton'
 import Base from 'app/assets/Base'
 import Strings from 'app/assets/Strings'
@@ -15,58 +15,100 @@ export default class SetingsView extends React.Component {
   }
 
   render() {
+    const track = Platform.OS === 'android' ? Base.colors.blue2 + '88' : Base.colors.blue2
+    const thumb = Platform.OS === 'android' ? Base.colors.blue2 : null
+
     return (
       <ScrollView style={styles.container}>
         <View style={styles.section}>
           <View style={styles.cell}>
             <Text style={styles.header}>General</Text>
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Private by default</Text>
-            <Switch onTintColor={Base.colors.blue2} />
+            <Switch
+              style={styles.switch}
+              thumbColor={thumb}
+              trackColor={{true: track}}
+            />
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Unread by default</Text>
-            <Switch onTintColor={Base.colors.blue2} />
+            <Switch
+              style={styles.switch}
+              thumbColor={thumb}
+              trackColor={{true: track}}
+            />
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Mark as read when opened</Text>
-            <Switch onTintColor={Base.colors.blue2} value={true} />
+            <Switch
+              style={styles.switch}
+              thumbColor={thumb}
+              trackColor={{true: track}}
+              value={true}
+            />
           </View>
+          <View style={styles.separator} />
         </View>
+
         <View style={styles.section}>
           <View style={styles.cell}>
             <Text style={styles.header}>Browser</Text>
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Default browser</Text>
             <Text style={styles.secondary}>Browser</Text>
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Open links in default browser</Text>
-            <Switch onTintColor={Base.colors.blue2} />
+            <Switch
+              trackColor={{true: track}}
+              thumbColor={thumb}
+              style={styles.switch}
+            />
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Mark as read when opened</Text>
-            <Switch onTintColor={Base.colors.blue2} />
+            <Switch
+              trackColor={{true: track}}
+              thumbColor={thumb}
+              style={styles.switch}
+            />
           </View>
+          <View style={styles.separator} />
         </View>
+
         <View style={styles.section}>
           <View style={styles.cell}>
             <Text style={styles.header}>Display</Text>
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Bold bookmark titles</Text>
             <Text style={styles.secondary}>Browser</Text>
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Exact dates</Text>
-            <Switch onTintColor={Base.colors.blue2} />
+            <Switch
+              style={styles.switch}
+              thumbColor={thumb}
+              trackColor={{true: track}}
+            />
           </View>
+          <View style={styles.separator} />
           <View style={styles.cell}>
             <Text style={styles.text}>Sort tags</Text>
             <Text style={styles.secondary}>Default</Text>
           </View>
+          <View style={styles.separator} />
         </View>
       </ScrollView>
     )
@@ -77,6 +119,12 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Base.colors.white,
   },
+  separator: {
+    borderTopColor: Base.colors.border,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderStyle: 'solid',
+    marginHorizontal: Base.padding.medium,
+  },
   section: {
     paddingTop: Base.padding.small,
   },
@@ -86,30 +134,30 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     height: 48,
-    marginHorizontal: Base.padding.medium,
-    borderBottomColor: Base.colors.border,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  active: {
-    backgroundColor: Base.colors.blue1,
   },
   text: {
     color: Base.colors.gray4,
     fontSize: Base.fonts.large,
+    paddingLeft: Base.padding.medium,
+  },
+  switch: {
+    marginRight: Platform.OS === 'android' ? 12 : Base.padding.medium,
   },
   secondary: {
     color: Base.colors.gray3,
     fontSize: Base.fonts.medium,
+    paddingRight: Base.padding.medium,
   },
   header: {
     color: Base.colors.gray4,
     fontSize: Base.fonts.large,
     fontWeight: Base.fonts.bold,
+    paddingHorizontal: Base.padding.medium,
   },
   version: {
     fontSize: Base.fonts.small,
     color: Base.colors.gray3,
     paddingVertical: Base.padding.large,
     paddingHorizontal: Base.padding.medium,
-  }
+  },
 })
