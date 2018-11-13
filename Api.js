13425796d63d49f5262577c5272d953f13092340
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-11-13 23:06:07 +0200

    Add API method for adding a post

--- Api.js
+++ Api.js
@@ -4,7 +4,8 @@ const server = 'https://api.pinboard.in/v1'
 
 const loginUrl = (parameters) => `${server}/user/api_token/?${parameters}`
 const updateUrl = (parameters) => `${server}/posts/update/?${parameters}`
-const postsUrl = (parameters) => `${server}/posts/all/?${parameters}`
+const postsAllUrl = (parameters) => `${server}/posts/all/?${parameters}`
+const postsAddUrl = (parameters) => `${server}/posts/add/?${parameters}`
 
 const handleResponse = (response) => {
   if (!response.ok) {
@@ -37,23 +38,39 @@ const update = (token) => {
   return fetchWithErrorHandling(updateUrl(params))
 }
 
-const posts = (token) => {
+const postsAll = (token) => {
   const data = {
     'format': 'json',
     'auth_token': token,
   }
   const params = queryString.stringify(data)
-  return fetchWithErrorHandling(postsUrl(params))
+  return fetchWithErrorHandling(postsAllUrl(params))
+}
+
+const postsAdd = (post, token) => {
+  const data = {
+    'url': post.href,
+    'description': post.description,
+    'extended': post.extended,
+    'tags': post.tags,
+    'toread': post.toread,
+    'shared': post.shared,
+    'format': 'json',
+    'auth_token': token,
+  }
+  const params = queryString.stringify(data)
+  return fetchWithErrorHandling(postsAddUrl(params))
 }
 
 const mockPosts = () => {
-  const mock = 'http://192.168.0.20:3000/posts.json'
+  const mock = 'http://192.168.0.18:3000/posts.json'
   return fetchWithErrorHandling(mock)
 }
 
 export default {
   login,
   update,
-  posts,
+  postsAll,
+  postsAdd,
   mockPosts,
 }
