e390336aac3d1c08524f393a7c1a3457356de316
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-11-19 23:30:41 +0200

    Use android back button on browser view instead of toolbar

--- views/BrowserView.js
+++ views/BrowserView.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { SafeAreaView, View, Image, WebView, StyleSheet, TouchableOpacity } from 'react-native'
+import { SafeAreaView, View, Image, WebView, StyleSheet, TouchableOpacity, Platform, BackHandler } from 'react-native'
 import PropTypes from 'prop-types'
 import CloseButton from 'app/components/CloseButton'
 import Base from 'app/assets/Base'
+
+const isAndroid = Platform.OS === 'android'
 
 export default class BrowserView extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -21,11 +23,60 @@ export default class BrowserView extends React.Component {
     }
   }
 
+  componentWillMount() {
+    if (isAndroid) {
+      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBack)
+    }
+  }
+
+  componentWillUnmount() {
+    if (isAndroid) {
+      BackHandler.removeEventListener('hardwareBackPress')
+    }
+  }
+
   onNavigationStateChange = (navState) => {
     this.setState({
       canGoBack: navState.canGoBack,
       canGoForward: navState.canGoForward,
     })
+  }
+
+  onAndroidBack = () => {
+    if (this.state.canGoBack) {
+      this.webview.goBack()
+      return true
+    }
+    return false
+  }
+
+  renderToolbar() {
+    return (
+      <View style={styles.toolbar}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          disabled={!this.state.canGoBack}
+          onPress={() => this.webview.goBack()}
+          style={styles.button}
+        >
+          <Image
+            source={require('app/assets/ic-left.png')}
+            style={[styles.icon, !this.state.canGoBack && styles.iconDisabled]}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          disabled={!this.state.canGoForward}
+          onPress={() => this.webview.goForward()}
+          style={styles.button}
+        >
+          <Image
+            source={require('app/assets/ic-right.png')}
+            style={[styles.icon, !this.state.canGoForward && styles.iconDisabled]}
+          />
+        </TouchableOpacity>
+      </View>
+    )
   }
 
   render() {
@@ -38,30 +89,7 @@ export default class BrowserView extends React.Component {
             startInLoadingState={true}
             onNavigationStateChange={this.onNavigationStateChange}
           />
-          <View style={styles.toolbar}>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              disabled={!this.state.canGoBack}
-              onPress={() => this.webview.goBack()}
-              style={styles.button}
-            >
-              <Image
-                source={require('app/assets/ic-left.png')}
-                style={[styles.icon, !this.state.canGoBack && styles.iconDisabled]}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              disabled={!this.state.canGoForward}
-              onPress={() => this.webview.goForward()}
-              style={styles.button}
-            >
-              <Image
-                source={require('app/assets/ic-right.png')}
-                style={[styles.icon, !this.state.canGoForward && styles.iconDisabled]}
-              />
-            </TouchableOpacity>
-          </View>
+          { isAndroid ? null : this.renderToolbar() }
         </View>
       </SafeAreaView>
     )
