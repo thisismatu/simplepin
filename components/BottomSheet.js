d8add8f030f05377e2cab6e4b8b82e1842fbb073
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-11-30 00:08:44 +0200

    Fix bottom sheet laggy animation

--- components/BottomSheet.js
+++ components/BottomSheet.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { StyleSheet, Modal, TouchableOpacity, View, Text, Platform } from 'react-native'
-import { SafeAreaView } from 'react-navigation'
+import { StyleSheet, Modal, TouchableOpacity, View, Text } from 'react-native'
+import { getBottomSpace } from 'react-native-iphone-x-helper'
 import PropTypes from 'prop-types'
 import Base from 'app/assets/Base'
 import Strings from 'app/assets/Strings'
-
-const BottomView = Platform.select({
-  ios: () => SafeAreaView,
-  android: () => View,
-})()
 
 const BottomSheet = (props) => {
   const toread = props.post.toread ? 'read' : 'unread'
@@ -25,48 +20,44 @@ const BottomSheet = (props) => {
         style={{ flex: 1 }}
         onPress={props.onClose}
       />
-      <BottomView style={styles.bottomSheet}>
-        <View style={styles.section}>
-          <View style={styles.cell}>
-            <Text
-              style={styles.title}
-              numberOfLines={1}
-              ellipsizeMode="tail"
-            >
-              {props.post.description}
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            activeOpacity={0.5}
-            style={styles.cell}
-            onPress={props.onToggleRead(props.post)}
+      <View style={styles.bottomSheet}>
+        <View style={styles.cell}>
+          <Text
+            style={styles.title}
+            numberOfLines={1}
+            ellipsizeMode="tail"
           >
-            <Text style={styles.text}>{Strings.common.markAs} {toread}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            activeOpacity={0.5}
-            style={styles.cell}
-          >
-            <Text style={styles.text}>{Strings.common.edit}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            activeOpacity={0.5}
-            style={styles.cell}
-            onPress={props.onDeletePost(props.post)}
-          >
-            <Text style={styles.text}>{Strings.common.delete}</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            activeOpacity={0.5}
-            style={styles.cell}
-            onPress={props.onClose}
-          >
-            <Text style={styles.text}>{Strings.common.cancel}</Text>
-          </TouchableOpacity>
+            {props.post.description}
+          </Text>
         </View>
-      </BottomView>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.cell}
+          onPress={props.onToggleRead(props.post)}
+        >
+          <Text style={styles.text}>{Strings.common.markAs} {toread}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.cell}
+        >
+          <Text style={styles.text}>{Strings.common.edit}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.cell}
+          onPress={props.onDeletePost(props.post)}
+        >
+          <Text style={styles.text}>{Strings.common.delete}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.cell}
+          onPress={props.onClose}
+        >
+          <Text style={styles.text}>{Strings.common.cancel}</Text>
+        </TouchableOpacity>
+      </View>
     </Modal>
   )
 }
@@ -82,18 +73,17 @@ BottomSheet.propTypes = {
 const styles = StyleSheet.create({
   bottomSheet: {
     backgroundColor: Base.color.white,
+    elevation: 8,
+    paddingBottom: Math.max(getBottomSpace(), Base.padding.small),
+    paddingTop: Base.padding.small,
     shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.15,
     shadowRadius: 4,
-    shadowOffset: { width: 0, height: 0 },
-    elevation: 8,
-  },
-  section: {
-    paddingVertical: Base.padding.small,
   },
   cell: {
-    flexDirection: 'row',
     alignItems: 'center',
+    flexDirection: 'row',
     height: Base.row.large,
     paddingHorizontal: Base.padding.medium,
   },
