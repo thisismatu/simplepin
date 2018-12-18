3d7a052c4c1bc0d6fff8add67d62a5b859998276
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-12-18 22:31:17 +0200

    Create icons file and add new icons

---
+++ assets/Icons.js
@@ -0,0 +1,16 @@
+const Icons = {
+  all: require('app/assets/ic-all.png'),
+  close: require('app/assets/ic-close.png'),
+  closeSmall: require('app/assets/ic-close-small.png'),
+  left: require('app/assets/ic-left.png'),
+  menu: require('app/assets/ic-menu.png'),
+  private: require('app/assets/ic-private.png'),
+  privateSmall: require('app/assets/ic-private-small.png'),
+  public: require('app/assets/ic-public.png'),
+  right: require('app/assets/ic-right.png'),
+  searchSmall: require('app/assets/ic-search-small.png'),
+  settings: require('app/assets/ic-settings.png'),
+  unread: require('app/assets/ic-unread.png'),
+}
+
+export default Icons
