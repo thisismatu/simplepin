7309c803480f22c32aaa345847a238a8bec4495f
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-12-24 00:02:35 +0200

    Convert navigation button to pure component

--- components/NavigationButton.js
+++ components/NavigationButton.js
@@ -2,17 +2,18 @@ import React from 'react'
 import { StyleSheet, TouchableOpacity, Image } from 'react-native'
 import PropTypes from 'prop-types'
 import Base from 'app/style/Base'
-import Icons from 'app/style/Icons'
 
-const NavigationButton = (props) => {
-  return (
-    <TouchableOpacity
-      activeOpacity={0.5}
-      onPress={props.onPress}
-    >
-      <Image source={props.icon} style={styles.menuButton} />
-    </TouchableOpacity>
-  )
+export default class NavigationButton extends React.PureComponent {
+  render() {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={this.props.onPress}
+      >
+        <Image source={this.props.icon} style={styles.menuButton} />
+      </TouchableOpacity>
+    )
+  }
 }
 
 NavigationButton.propTypes = {
@@ -27,5 +28,3 @@ const styles = StyleSheet.create({
     tintColor: Base.color.blue2,
   },
 })
-
-export default NavigationButton
