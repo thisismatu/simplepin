3bf5737c338b4e37a174bd29179e12089232cc09
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-12-24 00:43:29 +0200

    Convert time ago to pure component

--- components/TimeAgo.js
+++ components/TimeAgo.js
@@ -2,44 +2,47 @@ import React from 'react'
 import { Text } from 'react-native'
 import PropTypes from 'prop-types'
 
-const pluralize = (number, string) => {
-  if (number === 1) {
-    return `${number} ${string} ago`
+export default class TimeAgo extends React.PureComponent {
+  pluralize(number, string) {
+    if (number === 1) {
+      return `${number} ${string} ago`
+    }
+    return `${number} ${string}s ago`
   }
-  return `${number} ${string}s ago`
-}
 
-const format = (date, exactDate) => {
-  if (!date) { return null }
-  if (exactDate) { return date.toLocaleString() }
+  format(date, exactDate) {
+    if (!date) { return null }
+    if (exactDate) { return date.toLocaleString() }
 
-  const today = new Date()
-  const seconds = Math.floor((today - date) / 1000)
-  const minutes = Math.floor(seconds / 60)
-  const hours = Math.floor(minutes / 60)
-  const days = Math.floor(hours / 24)
-  const weeks = Math.floor(days / 7)
-  const options = { year: 'numeric', month: 'long' }
+    const today = new Date()
+    const seconds = Math.floor((today - date) / 1000)
+    const minutes = Math.floor(seconds / 60)
+    const hours = Math.floor(minutes / 60)
+    const days = Math.floor(hours / 24)
+    const weeks = Math.floor(days / 7)
+    const options = { year: 'numeric', month: 'long' }
 
-  if (seconds < 60) {
-    return 'just now'
-  } else if (minutes < 60) {
-    return pluralize(minutes, 'minute')
-  } else if (hours < 24) {
-    return pluralize(hours, 'hour')
-  } else if (days < 7) {
-    return pluralize(days, 'day')
-  } else if (weeks < 13) {
-    return pluralize(weeks, 'week')
-  } else {
-    return date.toLocaleDateString('en-EN', options)
+    if (seconds < 60) {
+      return 'just now'
+    } else if (minutes < 60) {
+      return this.pluralize(minutes, 'minute')
+    } else if (hours < 24) {
+      return this.pluralize(hours, 'hour')
+    } else if (days < 7) {
+      return this.pluralize(days, 'day')
+    } else if (weeks < 13) {
+      return this.pluralize(weeks, 'week')
+    } else {
+      return date.toLocaleDateString('en-EN', options)
+    }
   }
-}
 
-const TimeAgo = (props) => {
-  return (
-    <Text style={props.style}>{format(props.time, props.exactDate)}</Text>
-  )
+  render() {
+    const { style, time, exactDate } = this.props
+    return (
+      <Text style={style}>{this.format(time, exactDate)}</Text>
+    )
+  }
 }
 
 TimeAgo.propTypes = {
@@ -47,5 +50,3 @@ TimeAgo.propTypes = {
   exactDate: PropTypes.bool.isRequired,
   style: PropTypes.object,
 }
-
-export default TimeAgo
