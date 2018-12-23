1f6c1572da0c6a2c25044fe27f22e95545977c99
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-12-24 00:43:16 +0200

    Convert separator to pure component

--- components/Separator.js
+++ components/Separator.js
@@ -3,22 +3,22 @@ import { StyleSheet, View } from 'react-native'
 import PropTypes from 'prop-types'
 import Base from 'app/style/Base'
 
-const Separator = ({ left = Base.padding.medium, right = Base.padding.medium }) => {
-  return (
-    <View
-      style={{
-        marginRight: right,
-        marginLeft: left,
-        borderBottomColor: Base.color.black12,
-        borderBottomWidth: StyleSheet.hairlineWidth,
-      }}
-    />
-  )
+export default class Separator extends React.PureComponent {
+  render() {
+    return (
+      <View
+        style={{
+          marginRight: this.props.right,
+          marginLeft: this.props.left,
+          borderBottomColor: Base.color.black12,
+          borderBottomWidth: StyleSheet.hairlineWidth,
+        }}
+      />
+    )
+  }
 }
 
 Separator.propTypes = {
   left: PropTypes.number,
   right: PropTypes.number,
 }
-
-export default Separator
