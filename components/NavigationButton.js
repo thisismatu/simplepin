79a05647798e96bda20e0390c202938b5a9dbf94
Mathias Lindholm <mathias.lindholm@gmail.com>
2019-02-05 20:36:03 +0200

    Add text possibility to navigation button

--- components/NavigationButton.js
+++ components/NavigationButton.js
@@ -1,30 +1,46 @@
 import React from 'react'
-import { StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { StyleSheet, TouchableOpacity, Image, Text } from 'react-native'
 import PropTypes from 'prop-types'
 import Base from 'app/style/Base'
 
 export default class NavigationButton extends React.PureComponent {
   render() {
+    const { icon, text } = this.props
     return (
       <TouchableOpacity
         activeOpacity={0.5}
         onPress={this.props.onPress}
+        style={s.button}
       >
-        <Image source={this.props.icon} style={s.menuButton} />
+        {!!icon && <Image source={icon} style={s.icon} />}
+        {!!text && <Text style={s.text}>{text}</Text>}
       </TouchableOpacity>
     )
   }
 }
 
 NavigationButton.propTypes = {
-  icon: PropTypes.number.isRequired,
+  icon: PropTypes.number,
+  text: PropTypes.string,
   onPress: PropTypes.func,
 }
 
 const s = StyleSheet.create({
-  menuButton: {
-    marginHorizontal: 12,
-    marginVertical: Base.padding.small,
+  button: {
+    padding: Base.padding.small,
+  },
+  icon: {
     tintColor: Base.color.blue2,
+    resizeMode: 'contain',
+    marginHorizontal: 4,
+    width: 24,
+    height: 24,
+  },
+  text: {
+    color: Base.color.blue2,
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: Base.font.bold,
+    marginHorizontal: Base.padding.small,
   },
 })
