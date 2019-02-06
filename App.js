6d40897a30bc04ae9db6f85fac55ca2d233a43fd
Mathias Lindholm <mathias.lindholm@gmail.com>
2019-02-06 22:49:15 +0200

    Hide statusbar on landscape

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { StatusBar, Dimensions } from 'react-native'
 import AppNavigator from 'app/views/AppNavigator'
 
 export default class App extends React.Component {
   render() {
+    const { width, height } = Dimensions.get('window')
+    if (width > height) { StatusBar.setHidden(true) }
     return (
       <AppNavigator />
     )
