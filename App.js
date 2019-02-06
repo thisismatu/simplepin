6398a73eb7738c55a9f53bfa89d506c124a6b47b
Mathias Lindholm <mathias.lindholm@gmail.com>
2019-02-06 23:27:21 +0200

    Set white background for statusbar

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { StatusBar, Dimensions } from 'react-native'
+import { View, StatusBar } from 'react-native'
 import AppNavigator from 'app/views/AppNavigator'
 
 export default class App extends React.Component {
   render() {
-    const { width, height } = Dimensions.get('window')
-    if (width > height) { StatusBar.setHidden(true) }
     return (
-      <AppNavigator />
+      <View style={{ flex: 1 }}>
+        <StatusBar
+          animated={true}
+          backgroundColor="transparent"
+          barStyle="dark-content"
+          showHideTransition="fade"
+          translucent={true}
+        />
+        <AppNavigator />
+      </View>
     )
   }
 }
