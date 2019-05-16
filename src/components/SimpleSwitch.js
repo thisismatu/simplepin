1e5273c0beacd2b0ee772eb63e2bbe0f94e463d5
Mathias Lindholm <mathias.lindholm@gmail.com>
2019-05-17 00:20:52 +0300

    Remove custom track color method from switch component
    This was made when the app was stil inside expo. After ejecting we can set the android accentcolor wich is then used on android fro switches (and other ui components). iOS relies on the track color.

--- src/components/SimpleSwitch.js
+++ src/components/SimpleSwitch.js
@@ -3,18 +3,16 @@ import { Platform, StyleSheet, Switch } from 'react-native'
 import PropTypes from 'prop-types'
 import { color, padding } from '../style/style'
 
+const isIOS = Platform.OS === 'ios'
 const isAndroid = Platform.OS === 'android'
 
 export default class SimpleSwitch extends React.PureComponent {
   render() {
     const { value, onValueChange } = this.props
-    const track = isAndroid ? `${color.blue2}88` : color.blue2
-    const thumb = isAndroid && value ? color.blue2 : null
     return (
       <Switch
         style={s.switch}
-        thumbColor={thumb}
-        trackColor={{ true: track }}
+        trackColor={{ true: isIOS && color.blue2 }}
         onValueChange={onValueChange}
         value={value}
       />
